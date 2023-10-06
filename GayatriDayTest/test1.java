ae247e35cb47c0f8ac57abe92e80d153408b04df
Iamneo <techadmin@iamneo.ai>
2023-10-06 10:48:46 +0000

    Backup Commit

--- GayatriDayTest/test1.java
+++ GayatriDayTest/test1.java
@@ -1,5 +1,8 @@
 class test1{
     public static void main(String args[]){
+        String res="Hello";
+        StringBuffer sb = new StringBuffer(res);
+        String S=sb.reverse();
         System.out.println("Hello World !");
     }
 }
